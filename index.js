b44964314ce52f95896adc619dc61f19d2524d3a
ggurariy <grigoriy.gurariy@gmail.com>
2017-09-20 23:09:39 -0500

    changed hostname

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type' : 'text/plain'});
     res.write('Hello World!');
     res.end();
-}).listen(80, 'gurariy.com');
+}).listen(8000, '10.0.0.1');
