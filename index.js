9aeb1f9cc1b61dedd1408ac97bc4fea7fa7e8a94
NETWORK\ggurariy <ggurariy@hps.md>
2017-09-24 01:07:56 -0500

    exposed public endpoint for favicon & site.js + a couple other misc things

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./server/routes');
+//const routes = require('./server/routes');
 const handlebarsViewEngine = require('./server/handlebars-view-engine');
 
 const viewsPath = path.resolve(__dirname, './server/views');
 
 const app = express();
 
-app.engine('.handlebars', handlebarsViewEngine.setup(viewsPath));
+app.engine('handlebars', handlebarsViewEngine.setup(viewsPath));
 app.set('views', viewsPath);
 app.set('view engine', 'handlebars');
 
-routes(app);
+app.use('/public', express.static(path.resolve(__dirname, './public')));
+app.use('/home', require('./server/controllers/home'));
+app.use('/', require('./server/controllers/home'));
+
+//routes(app);
 
 app.listen(process.env.PORT, process.env.HOSTNAME, function() {  
     console.log('Express listening on port %d', process.env.PORT);
--- server/handlebars-view-engine.js
+++ server/handlebars-view-engine.js
@@ -5,8 +5,12 @@ module.exports = {
     setup: function(viewsPath) {
         const hbs = exphbs.create({ 
             defaultLayout: 'layout',
+            extname: '.handlebars',
             layoutsDir: path.resolve(viewsPath, './layouts'),
-            partialsDir: path.resolve(viewsPath, './partials')
+            partialsDir: path.resolve(viewsPath, './partials'),
+            helpers: {
+                scriptPath: '/public/scripts/site.js'
+            }
         });
         return hbs.engine;
     }
