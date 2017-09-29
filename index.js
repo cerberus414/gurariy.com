2f1af413f188b5a6fa8b3ae6563f8e2a3eea39fe
NETWORK\ggurariy <ggurariy@hps.md>
2017-09-29 16:33:22 -0500

    added additional comments

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const viewsPath = path.resolve(__dirname, './server/views');
 
 const app = express();
 
+// Register handlebars as a view engine
 app.engine('handlebars', handlebarsViewEngine.setup(viewsPath));
+// Set path for the views/templates so they can be found by the view engine
 app.set('views', viewsPath);
+// Register handlebars as the default view engine when view/template extension isn't specified in the controller
 app.set('view engine', 'handlebars');
 
 app.use('/public', express.static(path.resolve(__dirname, './public')));
