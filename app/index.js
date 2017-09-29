53ea897d2983a2239269aa8ced24abb7daec918f
NETWORK\ggurariy <ggurariy@hps.md>
2017-09-29 18:25:03 -0500

    About page & simple router

--- app/index.js
+++ app/index.js
@@ -1,8 +1,25 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const Home = require('./components/Home.jsx');
+const About = require('./components/About/About.jsx');
 
-// can also be written using JSX format: const home = <Home />;
-const home = React.createElement(Home, null);
+class App extends React.Component {
+    render() {
+        var Child;
 
-ReactDOM.render(home, document.querySelector('#container'));
+        switch(this.props.route) {
+            case 'about':  Child = About; break;
+            default: Child = Home;
+        }
+
+        return <Child/>;
+    }
+}
+
+const render = function() {
+    var route = window.location.hash.substr(1);
+    ReactDOM.render(<App route={route} />, document.querySelector('#container'));
+};
+
+window.addEventListener('hashchange', render);
+render();
